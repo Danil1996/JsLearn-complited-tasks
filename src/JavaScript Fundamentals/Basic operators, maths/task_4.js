9a47425e57f3f9496f59595e31d06511198a1606
Danil <danilkit48@gmail.com>
2021-10-12 18:38:28 +0300

    Well, I completed the part (Basic operators, maths)

---
+++ src/JavaScript Fundamentals/Basic operators, maths/task_4.js
@@ -0,0 +1,13 @@
+'use strict';
+// Here’s a code that asks the user for two numbers and shows their sum.
+// It works incorrectly. The output in the example below is 12 (for default prompt values).
+// Why? Fix it. The result should be 3.
+// let a = prompt("First number?", 1);
+// let b = prompt("Second number?", 2);
+
+// alert(a + b); // 12
+
+let a = '1'; //prompt('First number?', 1);
+let b = '2'; //prompt('Second number?', 2);
+
+console.log(+a + +b); // 3
