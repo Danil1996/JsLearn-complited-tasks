84f618daae0efdf1fc267811526ff9de26c91e97
Danil <danilkit48@gmail.com>
2021-10-12 22:32:28 +0300

    I createed the new branch(Object) and made the first part of the charapter

---
+++ src/Objects: the basics/Objects/task_4.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+// For instance:
+// // before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: 'My menu',
+};
+// multiplyNumeric(menu);
+// // after the call
+// menu = {
+//   width: 400,
+//   height: 600,
+//   title: "My menu"
+// };
+// Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
+// P.S. Use typeof to check for a number here.
+
+function multiplyNumeric(obj) {
+  for (const key in obj) {
+    let keyType = typeof obj[key];
+    if (keyType == 'number') {
+      obj[key] *= 2;
+    }
+  }
+  console.log(obj);
+}
+multiplyNumeric(menu);
