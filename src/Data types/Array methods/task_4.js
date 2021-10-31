030bbbc14343b04e0a9dff012b53e07f0259b174
Danil <danilkit48@gmail.com>
2021-10-31 17:54:27 +0200

    delete Data type folder

--- src/Data types/Array methods/task_4.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-let arr = [5, 2, 1, -10, 8];
-
-function arraySort(array) {
-  return array.sort((a, b) => b - a);
-}
-console.log(arraySort(arr)); // 8, 5, 2, 1, -10
--- src/Data types/String/task_1.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Write a function ucFirst(str) that returns the string str with the uppercased first character, for instance:
-function usFirst(string) {
-  let firstLeter = string[0].toUpperCase();
-  let newString = firstLeter + string.slice(1);
-  return newString;
-}
-console.log(usFirst('viktor'));
