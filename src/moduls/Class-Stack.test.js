cb20f44762f0f735750fc2ea317edc1d174e0bd2
Danil <danilkit48@gmail.com>
2021-12-14 00:43:39 +0200

    changed the location of the files, according to the review and made edits

--- src/moduls/Class-Stack.test.js
+++ src/moduls/Class-Stack.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const Stack = require('./moduls/Class-Stack.js');
+const Stack = require('./Class-Stack.js');
 const Chai = require('chai');
 const { assert } = require('chai');
 
@@ -15,29 +15,27 @@ describe('Class Stack implements the basic methods of the data structure stack',
     });
   });
 
-  describe('Method check (pop)', function () {
+  describe('Method check (pop)', function (element = 2) {
     it('Class method (pop) must delete top element in stack and return it', function () {
-      const element = 2;
       stack.push(element);
       assert.equal(stack.pop(), element);
-      it('Checking if the element has really been removed from the stack', function () {
-        assert.notEqual(stack.peek(), element);
-      });
+    });
+    it('Checking if the element has really been removed from the stack', function () {
+      assert.notEqual(stack.peek(), element);
       stack.pop();
     });
     it('Class method (pop) must throw an exception, if stack is empty', function () {
       assert.throw(() => stack.pop(), 'Stack is empty');
     });
   });
-  describe('Method check (peek)', function () {
+  describe('Method check (peek)', function (element = 3) {
     it('Class method (peek) must return top element of stack', function () {
-      const element = 3;
       stack.push(element);
-      const stackLength = stack.count();
       assert.equal(stack.peek(), element);
-      it('checking if the item is still in place', function () {
-        assert.equal(stack.count(), stackLength);
-      });
+    });
+    it('checking if the item is still in place', function () {
+      const stackLength = stack.count();
+      assert.equal(stack.count(), stackLength);
       stack.pop();
     });
     it('Class method (peek) must throw an exception, if stack is empty', function () {
