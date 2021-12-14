672134a9705a2fbcfa7439b8f4fcbaa63dc4fe6e
Danil <danilkit48@gmail.com>
2021-12-15 01:47:46 +0200

    change place for element and change test for peek method

--- src/modules/stack.test.js
+++ src/modules/stack.test.js
@@ -16,13 +16,13 @@ describe('Class Stack implements the basic methods of the data structure stack',
   });
 
   describe('Method check (pop)', () => {
+    const element = 2;
+
     it('Class method (pop) must delete top element in stack and return it', () => {
-      const element = 2;
       stack.push(element);
       assert.equal(stack.pop(), element);
     });
     it('Checking if the element has really been removed from the stack', () => {
-      const element = 2;
       assert.notEqual(stack.peek(), element);
       stack.pop();
     });
@@ -31,15 +31,13 @@ describe('Class Stack implements the basic methods of the data structure stack',
     });
   });
   describe('Method check (peek)', () => {
+    const element = 2;
     it('Class method (peek) must return top element of stack', () => {
-      const element = 2;
       stack.push(element);
       assert.equal(stack.peek(), element);
     });
     it('checking if the item is still in place', () => {
-      const stackLength = stack.count();
-      assert.equal(stack.count(), stackLength);
-      stack.pop();
+      assert.equal(stack.pop(), element);
     });
     it('Class method (peek) must throw an exception, if stack is empty', () => {
       assert.throw(() => stack.peek(), 'Stack is empty');
