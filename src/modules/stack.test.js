6be9ae3e62ed67f452f5ac3ff0670456be9dfc29
Danil <danilkit48@gmail.com>
2021-12-14 21:46:34 +0200

    added arrow functions and created variables directly in blocks

--- src/modules/stack.test.js
+++ src/modules/stack.test.js
@@ -6,44 +6,47 @@ const { assert } = require('chai');
 
 const stack = new Stack();
 
-describe('Class Stack implements the basic methods of the data structure stack', function () {
-  describe('Method check (push)', function () {
-    it('Class method (push) must add one element on th top of stack', function () {
+describe('Class Stack implements the basic methods of the data structure stack', () => {
+  describe('Method check (push)', () => {
+    it('Class method (push) must add one element on th top of stack', () => {
       const element = 1;
       stack.push(element);
       assert.equal(stack.peek(), element);
     });
   });
 
-  describe('Method check (pop)', function (element = 2) {
-    it('Class method (pop) must delete top element in stack and return it', function () {
+  describe('Method check (pop)', () => {
+    it('Class method (pop) must delete top element in stack and return it', () => {
+      const element = 2;
       stack.push(element);
       assert.equal(stack.pop(), element);
     });
-    it('Checking if the element has really been removed from the stack', function () {
+    it('Checking if the element has really been removed from the stack', () => {
+      const element = 2;
       assert.notEqual(stack.peek(), element);
       stack.pop();
     });
-    it('Class method (pop) must throw an exception, if stack is empty', function () {
+    it('Class method (pop) must throw an exception, if stack is empty', () => {
       assert.throw(() => stack.pop(), 'Stack is empty');
     });
   });
-  describe('Method check (peek)', function (element = 3) {
-    it('Class method (peek) must return top element of stack', function () {
+  describe('Method check (peek)', () => {
+    it('Class method (peek) must return top element of stack', () => {
+      const element = 2;
       stack.push(element);
       assert.equal(stack.peek(), element);
     });
-    it('checking if the item is still in place', function () {
+    it('checking if the item is still in place', () => {
       const stackLength = stack.count();
       assert.equal(stack.count(), stackLength);
       stack.pop();
     });
-    it('Class method (peek) must throw an exception, if stack is empty', function () {
+    it('Class method (peek) must throw an exception, if stack is empty', () => {
       assert.throw(() => stack.peek(), 'Stack is empty');
     });
   });
-  describe('Method check (count)', function () {
-    it('Class method (count) must return amount of elements', function () {
+  describe('Method check (count)', () => {
+    it('Class method (count) must return amount of elements', () => {
       const element = 1;
       stack.push(element);
       assert.equal(stack.count(), 1);
