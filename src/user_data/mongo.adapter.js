adc0764b05815827e024f275d8ee095cbca77903
Danil <danilkit48@gmail.com>
2022-03-11 14:53:30 +0200

    wrote documentation

--- src/user_data/mongo.adapter.js
+++ src/user_data/mongo.adapter.js
@@ -3,28 +3,48 @@
 module.exports = class MongoAdapter {
   #client;
 
+  /** mongo database adapter
+   * @constructor
+   * @param {object} client raised on server and connected mongo db client instance
+   */
   constructor(client) {
     this.#client = client;
   }
 
+  /**
+   * method create new user in mongo db
+   * @param {object} userObj object with userData
+   * @returns {boolean} boolean
+   */
   async create(userObj) {
     const result = await this.#client.insertOne(userObj);
     if (result.acknowledged) {
-      return result;
+      return true;
     }
-    return null;
+    return false;
   }
-
+  /**
+   * method returns users data by its id from mongo db
+   * @param {object} userId format object {id:'id'}
+   * @returns {(object\null)} users data or null
+   */
   async read(userId) {
     const result = await this.#client.findOne(userId);
+
     if (result) {
       return result;
     }
     return null;
   }
 
+  /**
+   * method returns array of the users objects by require data from mongo db
+   * @param {object} objUserFilter
+   * @returns {array{}} array of of searching users
+   */
   async readList(objUserFilter) {
     const result = this.#client.find(objUserFilter);
+    console.log('🚀 - MongoAdapter - result', result);
     const allValues = await result.toArray();
     if (result) {
       return allValues;
@@ -32,6 +52,12 @@ module.exports = class MongoAdapter {
     return [];
   }
 
+  /**
+   * method update an object from mongo db by userId and updatedData
+   * @param {object} userId format object {id:'id'}
+   * @param {object} data object with data to be replaced
+   * @returns {boolean} boolean
+   */
   async update(userId, data) {
     const result = await this.#client.updateOne(userId, { $set: data });
     if (result) {
@@ -40,8 +66,13 @@ module.exports = class MongoAdapter {
     return false;
   }
 
-  async delete(userId) {
-    const result = await this.#client.deleteOne(userId);
+  /**
+   * method removes an object from mongo db by some property
+   * @param {object} property format object {id:'id'}
+   * @returns {boolean} boolean
+   */
+  async delete(property) {
+    const result = await this.#client.deleteOne(property);
     if (result.deletedCount) {
       return true;
     }
--- src/user_data/user_data_module.js
+++ src/user_data/user_data_module.js
@@ -3,50 +3,86 @@
 module.exports = class UserDataModule {
   #storage;
 
+  /**
+   * interface for interacting user data with the database
+   * @constructor
+   * @param {object} storage database instance
+   */
   constructor(storage) {
     this.#storage = storage;
   }
 
+  /**
+   * method checking data and create user in a storage
+   * @param {object} objUser object with user data (firstName, lastName, age and id)
+   * @returns {(object|null)} user object or null
+   */
   async create(objUser) {
-    if (this.checkData(objUser)) {
+    if (this.#checkData(objUser)) {
       const creationResult = await this.#storage.create(objUser);
-      console.log('🚀 - UserDataModule - creationResult', creationResult);
-      return creationResult;
+      if (creationResult) {
+        return objUser;
+      }
     }
     return null;
   }
 
+  /**
+   * method returns a user object by its id
+   * @param {number} userId user id stored in the storage
+   * @returns {(object|null)} desired user object
+   */
   async read(userId) {
     const readingResult = await this.#storage.read({ id: userId.toString() });
-    console.log('🚀 - UserDataModule - readingResult', readingResult);
     return readingResult;
   }
 
+  /**
+   * method returns array of the users objects by require data
+   * @param {object} objUserFilter object with the required data
+   * @returns {array{}} array of searching users
+   */
   async readList(objUserFilter) {
     const readListResult = await this.#storage.readList(objUserFilter);
-    console.log('🚀 - UserDataModule - readListResult', readListResult);
     return readListResult;
   }
 
-  async update(userId, data) {
+  /**
+   * method update user data by id and updatedData in a storage
+   * @param {number} userId user id stored in the storage
+   * @param {object} updatedData object with data to be replaced in format {firstName:'string',lastName:'string', age: number, id: 'string'}
+   * @returns {boolean} boolean
+   */
+  async update(userId, updatedData) {
     if (
-      typeof data.firstName === 'string' ||
-      typeof data.lastName === 'string' ||
-      typeof data.age === 'number'
+      typeof updatedData.firstName === 'string' ||
+      typeof updatedData.lastName === 'string' ||
+      typeof updatedData.age === 'number'
     ) {
-      const updateResult = await this.#storage.update(userId.toString(), data);
-      console.log('🚀 - UserDataModule - updateResult', updateResult);
+      const updateResult = await this.#storage.update(
+        { id: userId.toString() },
+        updatedData
+      );
       return updateResult;
     }
+    return false;
   }
 
+  /**
+   * method deletes user data by id in a storage
+   * @param {number} userId user id stored in the storage
+   * @returns {boolean} boolean
+   */
   async delete(userId) {
     const deleteResult = await this.#storage.delete({ id: userId.toString() });
-    console.log('🚀 - UserDataModule - deleteResult', deleteResult);
     return deleteResult;
   }
-
-  checkData(data) {
+  /**
+   * method checking user data for a format {firstName:'string', lastName:'string', age:number,id:'string'}
+   * @param {object} data user data for checking the format
+   * @returns {boolean} boolean
+   */
+  #checkData(data) {
     if (
       typeof data.firstName === 'string' &&
       typeof data.lastName === 'string' &&
